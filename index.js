28e1273987dfa2331293c07c00906630adc01ffd
Example Name <itachikm123@gmail.com>
2025-02-10 17:21:42 +0700

    new router

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(methodOverride('_method'));
 // module.exports = app;
 //set up pug cho ứng dụng
 app.set('view engine', 'pug');
-app.set('views', 'D:/EKR22/view');
+app.set('views', process.cwd() + '/view');
 app.use('/', ex.static('./public'));
 var allRouter = require('./routers/sumaryRouter');
 allRouter(app);
